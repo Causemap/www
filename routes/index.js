d72d06c958da5d54188ce74446f5cd7af1cf0f26
Jeff Marshall <jeff@classy.co>
2014-10-11 22:25:33 -0700

    edit and use situation aliases

--- routes/index.js
+++ routes/index.js
@@ -5,85 +5,149 @@ var router = express.Router();
 var elasticsearch = require('elasticsearch');
 var marked = require('marked');
 
-var ES_URL = 'http://api.causemap.org:9200'
+// var ES_URL = 'http://api.causemap.org:9200';
+var ES_URL = 'http://elasticsearch-1.bbff670e-jeffmarshall.node.tutum.io:49153'
+var elasticsearch_client = new elasticsearch.Client({
+  host: ES_URL,
+  sniffOnConnectionFault: true
+});
 
 
-var get_situation = function(req, res, next, id){
-  var elasticsearch_client = new elasticsearch.Client({
-    host: ES_URL
-  });
-
+var get_situation = function(req, res, next){
   elasticsearch_client.get({
     index: 'situations',
     type: 'situation',
     id: req.params.situation_id
   }).then(function(result){
-    if (result._source.description){
-      result._source.html_description = marked(result._source.description, {
-        gfm: true
-      })
-      console.log(result._source.html_description)
+    req.situation = res.locals.situation = result._source;
+    return next();
+  }, function(error){
+    return next();
+  })
+}
+
+var get_aliased_situation = function(req, res, next){
+  if (req.situation) return next();
+
+  // search for the situation with this alias
+  elasticsearch_client.search({
+    index: 'changes',
+    type: 'situation.alias.change',
+    size: 1,
+    body: {
+      query: {
+        filtered: {
+          filter: {
+            term: {
+              'changed.field.to.untouched': req.params.situation_id
+            }
+          }
+        }
+      },
+      sort: [
+        { creation_date: { order: 'desc' } }
+      ]
+    }
+  }).then(function(result){
+    if (!result.hits.hits.length){
+      // nothing has ever had this alias
+      return next()
     }
 
-    req.situation = res.locals.situation = result._source;
-    res.locals.body_class = 'situation';
+    // get the situation that was aliased
+    var change = result.hits.hits[0]._source;
 
-    // get similar
-    var similar_query = { "query":
-      {
-        filtered: {
-          query: {
-            fuzzy_like_this: {
-              fields: ['name'],
-              like_text: req.situation.name,
-              max_query_terms: 12
-            }
-          },
-          filter: {
-            bool: {
-              must_not: [
-                {
-                  exists: {
-                    field: "marked_for_deletion"
-                  }
-                },
-                {
-                  ids: {
-                    type: 'situation',
-                    values: [ req.situation._id ]
-                  }
-                }
-              ],
-              must: {
+    elasticsearch_client.get({
+      index: 'situations',
+      type: 'situation',
+      id: change.changed.doc._id
+    }).then(function(hit){
+      var situation = hit._source;
+      req.situation = res.locals.situation = situation;
+
+      return next();
+    }, function(error){
+      return next(error)
+    })
+  })
+}
+
+var redirect_if_situation_alias = function(req, res, next){
+  if (!req.situation.alias || req.params.situation_id == req.situation.alias){
+    return next();
+  }
+
+  return res.redirect(301, '/'+ req.situation.alias)
+}
+
+var situation_or_404 = function(req, res, next){
+  if (req.situation) return next();
+
+  // 404 me bro
+  return res.send(404);
+}
+
+var markup_situation = function(req, res, next){
+  if (!req.situation || !req.situation.description) return next();
+
+  req.situation.html_description = marked(req.situation.description, {
+    gfm: true
+  })
+
+  return next();
+}
+
+var get_similar_situations = function(req, res, next){
+  if (!req.situation) return next();
+
+  var similar_query = { "query":
+    {
+      filtered: {
+        query: {
+          fuzzy_like_this: {
+            fields: ['name'],
+            like_text: req.situation.name,
+            max_query_terms: 12
+          }
+        },
+        filter: {
+          bool: {
+            must_not: [
+              {
                 exists: {
-                  field: "display_image"
+                  field: "marked_for_deletion"
                 }
+              },
+              {
+                ids: {
+                  type: 'situation',
+                  values: [ req.situation._id ]
+                }
+              }
+            ],
+            must: {
+              exists: {
+                field: "display_image"
               }
             }
           }
         }
       }
     }
+  }
 
-    elasticsearch_client.search({
-      index: 'situations',
-      type: 'situation',
-      body: similar_query
-    }).then(function(result){
-      req.similar = res.locals.similar = result
-      return next();
-    }, function(error){
-      console.log(error)
-      return next();
-    })
-
+  elasticsearch_client.search({
+    index: 'situations',
+    type: 'situation',
+    body: similar_query
+  }).then(function(result){
+    req.similar = res.locals.similar = result
+    return next();
   }, function(error){
-    return next(error)
+    console.log(error)
+    return next();
   })
 }
-
-router.param('situation_id', get_situation)
-
 
 router.get('/landing', function(req, res){
   return res.render('landing');
@@ -106,6 +170,11 @@ router.get('/', function(req, res) {
           bool: {
             must_not: {
               exists: { field: 'marked_for_deletion' }
+            },
+            must: {
+              exists: {
+                field: "display_image"
+              }
             }
           }
         }
@@ -141,70 +210,84 @@ router.get('/', function(req, res) {
 });
 
 router.get('/situation/:situation_id', function(req, res, next){
-  var elasticsearch_client = new elasticsearch.Client({
-    host: ES_URL
-  });
+  return res.redirect(301, '/'+ req.params.situation_id)
+})
 
-  var situation = req.situation;
+router.get(
+  '/:situation_id',
+  get_situation,
+  get_aliased_situation,
+  situation_or_404,
+  redirect_if_situation_alias,
+  markup_situation,
+  get_similar_situations,
+  function(req, res, next){
+    var elasticsearch_client = new elasticsearch.Client({
+      host: ES_URL
+    });
 
-  // get the first 3 causes and effects
-  async.map(['cause', 'effect'], function(rel_type, map_cb){
-    var q = {
-      index: 'relationships',
-      type: 'relationship',
-      size: 3,
-      body: {
-        sort: [
-          { strength: { order: 'desc' } },
-        ],
-        query: {
-          filtered: {
-            filter: {
-              bool: {
-                must_not: [
-                  { exists: { field: 'marked_for_deletion' } },
-                  { exists: { field: 'cause.marked_for_deletion'} },
-                  { exists: { field: 'effect.marked_for_deletion'} }
-                ],
-                must: {
-                  term: {},
+    var situation = req.situation;
+
+    // get the first 3 causes and effects
+    async.map(['cause', 'effect'], function(rel_type, map_cb){
+      var q = {
+        index: 'relationships',
+        type: 'relationship',
+        size: 3,
+        body: {
+          sort: [
+            { strength: { order: 'desc' } },
+          ],
+          query: {
+            filtered: {
+              filter: {
+                bool: {
+                  must_not: [
+                    { exists: { field: 'marked_for_deletion' } },
+                    { exists: { field: 'cause.marked_for_deletion'} },
+                    { exists: { field: 'effect.marked_for_deletion'} }
+                  ],
+                  must: {
+                    term: {},
+                  }
                 }
               }
             }
           }
         }
       }
-    }
 
-    q.body.query.filtered.filter.bool.must.term[rel_type +'._id'] = situation._id;
+      q.body.query.filtered.filter.bool.must.term[rel_type +'._id'] = situation._id;
 
-    elasticsearch_client.search(q).then(function(result){
-      var return_me = {};
-      return_me[rel_type == 'cause' ? 'effects' : 'causes'] = result.hits.hits.map(
-        function(hit){
-          return hit._source
-        }
-      )
+      elasticsearch_client.search(q).then(function(result){
+        var return_me = {};
+        return_me[rel_type == 'cause' ? 'effects' : 'causes'] = result.hits.hits.map(
+          function(hit){
+            return hit._source
+          }
+        )
 
-      return map_cb(null, return_me)
-    }, function(error){
-      return map_cb(error, null)
+        return map_cb(null, return_me)
+      }, function(error){
+        return map_cb(error, null)
+      })
+    }, function(map_error, map_results){
+      if (map_error){
+        console.error(map_error)
+        return res.send(500)
+      }
+
+      map_results.forEach(function(result){
+        situation = _.extend(situation, result)
+      })
+
+      res.render('overview', {
+        body_class: 'situation',
+        situation: situation
+      })
     })
-  }, function(map_error, map_results){
-    if (map_error){
-      console.error(map_error)
-      return res.send(500)
-    }
-
-    map_results.forEach(function(result){
-      situation = _.extend(situation, result)
-    })
-
-    res.render('overview', {
-      situation: situation
-    })
-  })
-})
+  }
+)
 
 
 router.get('/situation/:situation_id/:relationship_type', function(
