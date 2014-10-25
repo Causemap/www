90f9b3c0fb6aab37bdd94de554bd859a5766edee
Jeff Marshall <jeff@classy.co>
2014-10-24 19:40:16 -0700

    no more masonry landing page

--- routes/index.js
+++ routes/index.js
@@ -204,25 +204,27 @@ router.get('/', function(req, res) {
     }
 
     delete query.query.filtered.filter.bool.must.exists
+
+    return elasticsearch_client.search({
+      index: 'situations',
+      type: 'situation',
+      size: req.query.size || 25,
+      from: req.query.from || 0,
+      body: query
+    }).then(function(result){
+      res.render('index', {
+        query: req.query,
+        result: result,
+        size: parseInt(req.query.size) || 25,
+        from: parseInt(req.query.from) || 0
+      });
+
+    }, function(error){
+      res.send(500)
+    })
   }
 
-  elasticsearch_client.search({
-    index: 'situations',
-    type: 'situation',
-    size: req.query.size || 25,
-    from: req.query.from || 0,
-    body: query
-  }).then(function(result){
-    res.render('index', {
-      query: req.query,
-      result: result,
-      size: parseInt(req.query.size) || 25,
-      from: parseInt(req.query.from) || 0
-    });
-
-  }, function(error){
-    res.send(500)
-  })
+  return res.render('index');
 });
 
 router.get('/situation/:situation_id', function(req, res, next){
